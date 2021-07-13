8e03558c52f30080dcac4ac981e9d7ab123e52e5
chris7 <chris@thehardys.net>
2021-07-13 09:59:15 +0000

    after adding .htaccess

--- z7TsL3elmXEA7SKEtBfd8BFTB04Cudlwz/php/funcsForAccCcc.php
+++ z7TsL3elmXEA7SKEtBfd8BFTB04Cudlwz/php/funcsForAccCcc.php
@@ -2473,7 +2473,7 @@ function calJavaScrpInteractnLite($uniqueId, $viewOnly, $outerDivClass, $outerDi
     $mnthTextBoxName = $uniqueId."month";
     $mnthTextBoxId = $mnthUniqueId.'textBx';
     //-----
-    $yrsAllAry = array("2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021");
+    $yrsAllAry = array("2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022");
     $baseYr = $yrsAllAry[0];
     $yrMaxId = sizeof($yrsAllAry) -1;
     $yrUniqueId = $uniqueId."-yr";
